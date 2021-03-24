2b1188cab480150f0e2d4d957ca77bb5b3320d33
슬기 <seulgi@seulgiui-MacBookPro.local>
2021-03-24 09:43:09 +0900

    feat:Stock 테이블 및 Dto생성

---
+++ be/demo/src/main/java/com/example/demo/stock/repository/ItemRepository.java
@@ -0,0 +1,5 @@
+package com.example.demo.stock.repository;
+
+public interface ItemRepository {
+
+}
---
+++ be/demo/src/main/java/com/example/demo/stock/service/ItemServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.demo.stock.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ItemServiceImpl implements ItemService {
+
+}

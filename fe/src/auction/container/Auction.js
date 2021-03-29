9728dce8b00c016fdf08277e7cbdd11abeedbd60
may-hj <hyeonjeong9943@gmail.com>
2021-03-29 21:30:57 +0900

    Basic codes

---
+++ fe/src/auction/container/Auction.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import axios from 'axios'
+import '../component/Auction.css'
+
+const Auction = () => {
+    
+  return (<>
+    <h1>Hello Project</h1>
+  </>)
+}
+
+export default Auction
